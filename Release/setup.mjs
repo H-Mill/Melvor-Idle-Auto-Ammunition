6a2f539ea8e330358ab69b619dc2c4b17e3bbaf0
Hunter Miller <140934521+H-Mill@users.noreply.github.com>
2023-09-04 09:34:12 -0400

    9/4/2023 game update
    add mod toggle setting, fix javelin ID, add try/catch to prevent game crashes on errors, add toasts

--- Release/setup.mjs
+++ Release/setup.mjs
@@ -1,120 +1,178 @@
 const constant = {
-    modTitle: 'Auto Ammo Mod',
-    verson: 1.2
+	modTitle: "Auto Ammo Mod",
+	verson: 1.3,
+	modEnabledId: "mod-enabled",
 };
 const { modTitle } = constant;
 const ctx = mod.getContext(import.meta);
 let ammoData;
 let swapType = null;
 
-function log (msg) {
-    console.log(`[${modTitle}]: ${msg}`);
+function log(msg) {
+	console.log(`[${modTitle}]: ${msg}`);
 }
 
-async function populateAmmoData(loadData){
-    ammoData = await loadData('src/ammoData.json');
-    ammoData = new Map(Object.entries(ammoData));
-    for(let i = 0; i < ammoData.size; i++){
-        let ammoDataType = ammoData.get(i+'');
-        for(let j = 0; j < ammoDataType.length; j++){
-            let item = game.items.getObjectByID(ammoDataType[j+'']);
-            ammoDataType[j] = item;
-        }
-        ammoData.set(i+'', ammoDataType);
-    }
+async function populateAmmoData(loadData) {
+	ammoData = await loadData("src/ammoData.json");
+	ammoData = new Map(Object.entries(ammoData));
+	for (let i = 0; i < ammoData.size; i++) {
+		let ammoDataType = ammoData.get(i + "");
+		for (let j = 0; j < ammoDataType.length; j++) {
+			let item = game.items.getObjectByID(ammoDataType[j + ""]);
+			ammoDataType[j] = item;
+		}
+		ammoData.set(i + "", ammoDataType);
+	}
 }
 
 function setSwapType(typeValue) {
-    swapType = typeValue + '';
+	swapType = typeValue + "";
 }
 
 function getSwapType() {
-    return swapType;
+	return swapType;
 }
 
 function shouldAutoEquip() {
-    return swapType !== null;
+	return swapType !== null;
 }
 
 function resetSwapType() {
-    swapType = null;
+	swapType = null;
 }
 
-function getAmmoType(){
-    const equipmentSlots = game.combat.player.equipment.slots;
-    return equipmentSlots.Quiver.item.ammoType 
-        ?? equipmentSlots.Weapon.item.ammoTypeRequired
-        ?? getSwapType();
+function getAmmoType() {
+	const equipmentSlots = game.combat.player.equipment.slots;
+	return (
+		equipmentSlots.Weapon.item.ammoTypeRequired ??
+		equipmentSlots.Quiver.item.ammoType ??
+		getSwapType()
+	);
 }
 
 function isRangedAttack() {
-    const weapon = game.combat.player.equipment.slots.Weapon;
-    return weapon.item.attackType === 'ranged';
+	const weapon = game.combat.player.equipment.slots.Weapon;
+	return weapon.item.attackType === "ranged";
 }
 
 function autoEquipAmmo() {
-    if(!isRangedAttack())
-        return;
-        
-    if(!shouldAutoEquip())
-        return;
+	const ammoType = getAmmoType();
+	const ammoChoices = ammoData
+		.get(ammoType + "")
+		.filter(
+			(c) =>
+				c.equipRequirements !== null &&
+				c.equipRequirements !== undefined &&
+				c.equipRequirements.find((r) => r.level <= game.ranged.level)
+		);
 
-    const ammoType = getAmmoType();
-    const ammoChoices = ammoData.get(ammoType+'')
-        .filter(c => c.equipRequirements !== null 
-             && c.equipRequirements !== undefined 
-             && c.equipRequirements.find(r => r.level <= game.ranged.level));
+	for (let i = 0; i < ammoChoices.length; i++) {
+		const ammoChoice = ammoChoices[i];
+		if (game.bank.hasItem(ammoChoice)) {
+			const ownedQty = game.bank.getQty(ammoChoice);
+			game.combat.player.equipItem(
+				ammoChoice,
+				game.combat.player.selectedEquipmentSet,
+				"Default",
+				ownedQty
+			);
+			break;
+		}
+	}
 
-    for(let i = 0; i < ammoChoices.length; i++){
-        const ammoChoice = ammoChoices[i];
-        if(game.bank.hasItem(ammoChoice)){
-            const ownedQty = game.bank.getQty(ammoChoice);
-            game.combat.player.equipItem(ammoChoice, game.combat.player.selectedEquipmentSet, 'Default', ownedQty);
-            break;
-        }
-    }
+	resetSwapType();
+}
 
-    resetSwapType();
+function error(msg) {
+	toast(
+		`${constant.modTitle} experienced an error. It will be fixed ASAP, ${game.characterName} :). If this error persists go ahead and disable the mod via settings until a fix is available!`,
+		5000
+	);
+	log(`issue during initializeForCombat: ${msg}`);
+}
+
+function toast(text, duration = 2000) {
+	Toastify({
+		text,
+		duration,
+		gravity: "bottom",
+		position: "center",
+		backgroundColor: "black",
+		stopOnFocus: false,
+	}).showToast();
+}
+
+function isModEnabled() {
+	return ctx.settings.section("General").get(constant.modEnabledId);
 }
 
 export async function setup({ loadData }) {
-    populateAmmoData(loadData);
+	ctx.settings.section("General").add({
+		type: "switch",
+		name: constant.modEnabledId,
+		label: "Enable Mod",
+		hint: "Automatically equip ammo before/during combat for the equipped ranged weapon.",
+		default: true,
+	});
 
-    ctx.patch(Character, 'initializeForCombat').after(function() {
-        if(!isRangedAttack())
-            return;
+	try {
+		populateAmmoData(loadData);
+	} catch (e) {
+		log(`issue loading ammo data: ${e}`);
+	}
 
-        const weapon = game.combat.player.equipment.slots.Weapon;
-        const quiver = game.combat.player.equipment.slots.Quiver;
-        if(weapon.item.ammoTypeRequired !== 0 && quiver.quantity >= 1
-            || weapon.item.ammoTypeRequired !== 1 && quiver.quantity >= 1)
-            return;
+	ctx.patch(Character, "initializeForCombat").after(function () {
+		try {
+			if (!isModEnabled) return;
+			if (!isRangedAttack()) return;
 
-        setSwapType(getAmmoType());
-        autoEquipAmmo();
-    });
+			const weapon = game.combat.player.equipment.slots.Weapon;
+			const quiver = game.combat.player.equipment.slots.Quiver;
+			if (
+				(weapon.item.ammoTypeRequired !== 0 &&
+					quiver.item.ammoType !== 0 &&
+					quiver.quantity >= 1) ||
+				(weapon.item.ammoTypeRequired !== 1 &&
+					quiver.item.ammoType !== 1 &&
+					quiver.quantity >= 1)
+			)
+				return;
 
-    ctx.patch(Player, 'attack').before(function() {
-        if(!isRangedAttack())
-                return;
+			setSwapType(getAmmoType());
+			autoEquipAmmo();
+		} catch (e) {
+			error(`issue during initializeForCombat: ${e}`);
+		}
+	});
 
-        const autoAmmoPurchased = [...game.shop.upgradesPurchased.keys()]
-            .some(p => p._localID === 'hm_auto_ammo_upgrade');
-        if(!autoAmmoPurchased)
-            return;
+	ctx.patch(Player, "attack").before(function () {
+		try {
+			if (!isModEnabled) return;
+			if (!isRangedAttack()) return;
 
-        const quiver = game.combat.player.equipment.slots.Quiver;
-        if(quiver.quantity > 1)
-            return;
+			const autoAmmoPurchased = [
+				...game.shop.upgradesPurchased.keys(),
+			].some((p) => p._localID === "hm_auto_ammo_upgrade");
+			if (!autoAmmoPurchased) return;
 
-        setSwapType(getAmmoType());
-    });
+			const quiver = game.combat.player.equipment.slots.Quiver;
+			if (quiver.quantity > 1) return;
 
-    ctx.patch(Player, 'attack').after(function() {
-        if(!isRangedAttack())
-            return;
-        autoEquipAmmo();
-    });
-    log('loaded!');
+			setSwapType(getAmmoType());
+		} catch (e) {
+			error(`issue during attack(before): ${e}`);
+		}
+	});
+
+	ctx.patch(Player, "attack").after(function () {
+		try {
+			if (!isModEnabled) return;
+			if (!shouldAutoEquip()) return;
+
+			autoEquipAmmo();
+		} catch (e) {
+			error(`issue during attack(after): ${e}`);
+		}
+	});
+	log("loaded!");
 }
-
