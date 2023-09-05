77c8dd13326436552ee129c40a91d090fb974325
Hunter Miller <140934521+H-Mill@users.noreply.github.com>
2023-09-05 19:58:37 -0400

    refactor code, formatting

--- Release/setup.mjs
+++ Release/setup.mjs
@@ -1,11 +1,17 @@
 const constant = {
 	modTitle: "Auto Ammo Mod",
 	verson: 1.4,
-	modEnabledId: "mod-enabled",
-	enableArrowSwapId: "enable-arrow-swap",
-	enableBoltSwapId: "enable-bolt-swap",
-	enableKnifeSwapId: "enable-knife-swap",
-	enableJavelinSwapId: "enable-javelin-swap",
+	id: {
+		storage: "auto-ammo-storage",
+		modEnabledId: "mod-enabled",
+		enableArrowSwapId: "enable-arrow-swap",
+		enableBoltSwapId: "enable-bolt-swap",
+		enableKnifeSwapId: "enable-knife-swap",
+		enableJavelinSwapId: "enable-javelin-swap",
+	},
+	friendlyTitle: {
+		mod: "Auto Ammunition",
+	},
 	ammoTypeMap: {
 		arrow: "0",
 		bolt: "1",
@@ -13,64 +19,150 @@ const constant = {
 		knife: "3",
 	},
 };
-const { modTitle } = constant;
 const ctx = mod.getContext(import.meta);
 let ammoData;
+let storedData = {
+	upgradePurchased: false,
+	arrowSwapEnabled: false,
+	boltSwapEnabled: false,
+	knifeSwapEnabled: false,
+	javelinSwapEnabled: false,
+};
 let swapType = null;
-let combatMenuIcon = null;
 
-export async function setup({ loadData, onInterfaceReady }) {
-	setupSettings();
+export async function setup({
+	onInterfaceReady,
+	onCharacterLoaded,
+	loadData,
+	characterStorage,
+}) {
 	try {
+		setupSettings();
 		populateAmmoData(loadData);
+		managePurchaseData(characterStorage);
+		ctx.patch(Character, "initializeForCombat").after(
+			onInitializeForCombat
+		);
+		ctx.patch(Player, "attack").before(onBeforeAttack);
+		ctx.patch(Player, "attack").after(onAfterAttack);
+		onInterfaceReady(onUiReady);
+		onCharacterLoaded(() => onCharLoaded(characterStorage));
+		log("loaded!");
 	} catch (e) {
-		log(`issue loading ammo data: ${e}`);
+		log(e);
 	}
-	ctx.patch(Character, "initializeForCombat").after(onInitializeForCombat);
-	ctx.patch(Player, "attack").before(onBeforeAttack);
-	ctx.patch(Player, "attack").after(onAfterAttack);
+}
 
-	onInterfaceReady(() => {
-		const getColoredHtml = (text, success = false, successModifier = 'Enabled', failureModifier = 'Disabled') => `<p class="text-${success ? 'success' : 'danger'}">${text}: ${success ? successModifier : failureModifier}</p>`;
-		const formatInactiveTextIfNeeded = isPurchased => ('Enabled' + (!isPurchased ? ' <span class="text-danger">(Inactive)</span>' : ''));
-		let iconUrl = ctx.getResourceUrl('assets/ammo-pouch.png');
-		let purchasedElement = document.createElement('img');
-		purchasedElement.id = 'combat-menu-item-auto-ammo';
-		purchasedElement.src = iconUrl;
-		purchasedElement.className = 'combat-equip-img border-rounded-equip p-1 m-1 pointer-enabled'
-		purchasedElement.onclick = () => {
-			const _isModEnabled = isModEnabled();
-			const _isAutoAmmoPurchased = isAutoAmmoPurchased();
-			const _shouldAutoSwapArrows = shouldAutoSwapArrows();
-			const _shouldAutoSwapBolts = shouldAutoSwapBolts();
-			const _shouldAutoSwapKnives = shouldAutoSwapKnives();
-			const _shouldAutoSwapJavelins = shouldAutoSwapJavelins();
-			Swal.fire({
-				title: 'Auto Ammunition',
-				icon: iconUrl,
-				html: '<i style="font-size: 0.75rem;">These settings can be modified in the Mod Settings sidebar.</i>'
-					+ `<p>${getColoredHtml('Mod', _isModEnabled)}</p>`
-					+ `<p>${getColoredHtml('Upgrade', _isAutoAmmoPurchased, 'Purchased', 'Not Purchased')}</p>`
-					+ `<p>${getColoredHtml('Auto Arrows', _shouldAutoSwapArrows, formatInactiveTextIfNeeded(_isAutoAmmoPurchased))}</p>`
-					+ `<p>${getColoredHtml('Auto Bolts', _shouldAutoSwapBolts, formatInactiveTextIfNeeded(_isAutoAmmoPurchased))}</p>`
-					+ `<p>${getColoredHtml('Auto Knives', _shouldAutoSwapKnives, formatInactiveTextIfNeeded(_isAutoAmmoPurchased))}</p>`
-					+ `<p>${getColoredHtml('Auto Javelins', _shouldAutoSwapJavelins, formatInactiveTextIfNeeded(_isAutoAmmoPurchased))}</p>`,
-				showCloseButton: true
-			});
-		}
-	
-		const combatMenuItems = document.querySelectorAll('[id^=combat-menu-item-]');
-		const lastMenuItem = combatMenuItems[combatMenuItems.length-1];
-		lastMenuItem.after(purchasedElement);
-		combatMenuIcon = purchasedElement;
-		tippy(purchasedElement, {
-			content: 'Auto Ammunition',
-			placement: 'bottom',
-			interactive: false,
-			animation: false,
+function onUiReady() {
+	const { friendlyTitle } = constant;
+	const getColoredHtml = (
+		text,
+		success = false,
+		successModifier = "Enabled",
+		failureModifier = "Disabled"
+	) =>
+		`<p class="text-${success ? "success" : "danger"}">${text}: ${
+			success ? successModifier : failureModifier
+		}</p>`;
+	const formatInactiveTextIfNeeded = (isPurchased) =>
+		"Enabled" +
+		(!isPurchased ? ' <span class="text-danger">(Inactive)</span>' : "");
+	let iconUrl = ctx.getResourceUrl("assets/ammo-pouch.png");
+	let purchasedElement = document.createElement("img");
+	purchasedElement.id = "combat-menu-item-auto-ammo";
+	purchasedElement.src = iconUrl;
+	purchasedElement.className =
+		"combat-equip-img border-rounded-equip p-1 m-1 pointer-enabled";
+	purchasedElement.onclick = () => {
+		const _isModEnabled = isModEnabled();
+		const _isAutoAmmoPurchased = isAutoAmmoPurchased();
+		const _shouldAutoSwapArrows = shouldAutoSwapArrows();
+		const _shouldAutoSwapBolts = shouldAutoSwapBolts();
+		const _shouldAutoSwapKnives = shouldAutoSwapKnives();
+		const _shouldAutoSwapJavelins = shouldAutoSwapJavelins();
+		Swal.fire({
+			title: friendlyTitle.mod,
+			icon: iconUrl,
+			html:
+				'<i style="font-size: 0.75rem;">These settings can be modified in the Mod Settings sidebar.</i>' +
+				`<p>${getColoredHtml("Mod", _isModEnabled)}</p>` +
+				`<p>${getColoredHtml(
+					"Upgrade",
+					_isAutoAmmoPurchased,
+					"Purchased",
+					"Not Purchased"
+				)}</p>` +
+				`<p>${getColoredHtml(
+					"Auto Arrows",
+					_shouldAutoSwapArrows,
+					formatInactiveTextIfNeeded(_isAutoAmmoPurchased)
+				)}</p>` +
+				`<p>${getColoredHtml(
+					"Auto Bolts",
+					_shouldAutoSwapBolts,
+					formatInactiveTextIfNeeded(_isAutoAmmoPurchased)
+				)}</p>` +
+				`<p>${getColoredHtml(
+					"Auto Knives",
+					_shouldAutoSwapKnives,
+					formatInactiveTextIfNeeded(_isAutoAmmoPurchased)
+				)}</p>` +
+				`<p>${getColoredHtml(
+					"Auto Javelins",
+					_shouldAutoSwapJavelins,
+					formatInactiveTextIfNeeded(_isAutoAmmoPurchased)
+				)}</p>`,
+			showCloseButton: true,
 		});
-    });
-	log("loaded!");
+	};
+
+	const combatMenuItems = document.querySelectorAll(
+		"[id^=combat-menu-item-]"
+	);
+	const lastMenuItem = combatMenuItems[combatMenuItems.length - 1];
+	lastMenuItem.after(purchasedElement);
+	tippy(purchasedElement, {
+		content: friendlyTitle.mod,
+		placement: "bottom",
+		interactive: false,
+		animation: false,
+	});
+}
+
+function onCharLoaded(characterStorage) {
+	managePurchaseData(characterStorage);
+}
+
+function onInitializeForCombat() {
+	if (!isModEnabled()) return;
+	if (!isAutoAmmoPurchased()) return;
+	if (!isRangedAttack()) return;
+	if (!settingShouldAutoSwap()) return;
+	if (!shouldEquipAmmoOnInitializeCombat()) return;
+
+	setSwapType(getAmmoType());
+	autoEquipAmmo();
+}
+
+function onBeforeAttack() {
+	if (!isModEnabled()) return;
+	if (!isAutoAmmoPurchased()) return;
+	if (!isRangedAttack()) return;
+	if (!settingShouldAutoSwap()) return;
+
+	const quiver = getQuiver();
+	if (quiver.quantity > 1) return;
+
+	setSwapType(getAmmoType());
+}
+
+function onAfterAttack() {
+	if (!isModEnabled()) return;
+	if (!isAutoAmmoPurchased()) return;
+	if (!shouldAutoEquip()) return;
+	if (!settingShouldAutoSwap()) return;
+
+	autoEquipAmmo();
 }
 
 function setupSettings() {
@@ -80,7 +172,7 @@ function setupSettings() {
 		enableBoltSwapId,
 		enableKnifeSwapId,
 		enableJavelinSwapId,
-	} = constant;
+	} = constant.id;
 	const generalSection = ctx.settings.section("General");
 	generalSection.add({
 		type: "switch",
@@ -123,75 +215,26 @@ function setupSettings() {
 	});
 }
 
-function onInitializeForCombat() {
-	try {
-		if (!isModEnabled()) return;
-		if (!isAutoAmmoPurchased()) return;
-		if (!isRangedAttack()) return;
-		if (!settingShouldAutoSwap()) return;
-		if (!shouldEquipAmmoOnInitializeCombat()) return;
-
-		setSwapType(getAmmoType());
-		autoEquipAmmo();
-	} catch (e) {
-		error(`issue during initializeForCombat: ${e}`);
-	}
-}
-
 function shouldEquipAmmoOnInitializeCombat() {
-	const quiver = game.combat.player.equipment.slots.Quiver;
 	const ammoType = getAmmoType();
 	const { arrow, bolt, javelin, knife } = constant.ammoTypeMap;
 	if (ammoType === javelin) return false;
 	if (ammoType === knife) return false;
 
+	const quiver = getQuiver();
 	if (ammoType === arrow) {
-		if(equippedAmmoMatchesType(arrow))
-			return quiver.quantity === 0;
+		if (equippedAmmoMatchesType(arrow)) return quiver.quantity === 0;
+		return true;
+	} else if (ammoType === bolt) {
+		if (equippedAmmoMatchesType(bolt)) return quiver.quantity === 0;
 		return true;
 	}
-	else if (ammoType === bolt){
-		if(equippedAmmoMatchesType(bolt))
-			return quiver.quantity === 0;
-		return true;
-	} 
 
 	return false;
 }
 
 function equippedAmmoMatchesType(expectedAmmoType) {
-	const quiver = game.combat.player.equipment.slots.Quiver;
-	return quiver.item.ammoType === expectedAmmoType;
-}
-
-function onBeforeAttack() {
-	try {
-		if (!isModEnabled()) return;
-		if (!isAutoAmmoPurchased()) return;
-		if (!isRangedAttack()) return;
-		if (!settingShouldAutoSwap()) return;
-		
-
-		const quiver = game.combat.player.equipment.slots.Quiver;
-		if (quiver.quantity > 1) return;
-
-		setSwapType(getAmmoType());
-	} catch (e) {
-		error(`issue during attack(before): ${e}`);
-	}
-}
-
-function onAfterAttack() {
-	try {
-		if (!isModEnabled()) return;
-		if (!isAutoAmmoPurchased()) return;
-		if (!shouldAutoEquip()) return;
-		if (!settingShouldAutoSwap()) return;
-
-		autoEquipAmmo();
-	} catch (e) {
-		error(`issue during attack(after): ${e}`);
-	}
+	return getQuiver().item.ammoType === expectedAmmoType;
 }
 
 async function populateAmmoData(loadData) {
@@ -205,6 +248,17 @@ async function populateAmmoData(loadData) {
 		}
 		ammoData.set(i + "", ammoDataType);
 	}
+}
+
+function managePurchaseData(characterStorage) {
+	const { storage } = constant.id;
+	let loadedStorage = characterStorage.getItem(storage);
+	if (loadedStorage !== undefined) {
+		storedData = loadedStorage;
+		return;
+	}
+
+	characterStorage.setItem(storage, storedData);
 }
 
 function isAutoAmmoPurchased() {
@@ -230,17 +284,15 @@ function resetSwapType() {
 }
 
 function getAmmoType() {
-	const equipmentSlots = game.combat.player.equipment.slots;
 	let type =
-		equipmentSlots.Weapon.item.ammoTypeRequired ??
-		equipmentSlots.Quiver.item.ammoType ??
+		getWeapon().item.ammoTypeRequired ??
+		getQuiver().item.ammoType ??
 		getSwapType();
 	return type + "";
 }
 
 function isRangedAttack() {
-	const weapon = game.combat.player.equipment.slots.Weapon;
-	return weapon.item.attackType === "ranged";
+	return getWeapon().item.attackType === "ranged";
 }
 
 function autoEquipAmmo() {
@@ -287,23 +339,23 @@ function getGeneralSettings() {
 }
 
 function isModEnabled() {
-	return getGeneralSettings().get(constant.modEnabledId);
+	return getGeneralSettings().get(constant.id.modEnabledId);
 }
 
 function shouldAutoSwapArrows() {
-	return getGeneralSettings().get(constant.enableArrowSwapId);
+	return getGeneralSettings().get(constant.id.enableArrowSwapId);
 }
 
 function shouldAutoSwapBolts() {
-	return getGeneralSettings().get(constant.enableBoltSwapId);
+	return getGeneralSettings().get(constant.id.enableBoltSwapId);
 }
 
 function shouldAutoSwapKnives() {
-	return getGeneralSettings().get(constant.enableKnifeSwapId);
+	return getGeneralSettings().get(constant.id.enableKnifeSwapId);
 }
 
 function shouldAutoSwapJavelins() {
-	return getGeneralSettings().get(constant.enableJavelinSwapId);
+	return getGeneralSettings().get(constant.id.enableJavelinSwapId);
 }
 
 function settingShouldAutoSwap() {
@@ -317,14 +369,24 @@ function settingShouldAutoSwap() {
 	return false;
 }
 
+function getQuiver() {
+	return game.combat.player.equipment.slots.Quiver;
+}
+
+function getWeapon() {
+	return game.combat.player.equipment.slots.Weapon;
+}
+
 function log(msg) {
+	const { modTitle } = constant;
 	console.log(`[${modTitle}]: ${msg}`);
 }
 
 function error(msg) {
+	const { modTitle } = constant;
 	toast(
-		`${constant.modTitle} experienced an error. It will be fixed ASAP, ${game.characterName} :). If this error persists go ahead and disable the mod via settings until a fix is available!`,
-		5000
+		`${modTitle} experienced an error. It will be fixed ASAP, ${game.characterName} :). If this error persists go ahead and disable the mod via settings until a fix is available!`,
+		0
 	);
-	log(`issue during initializeForCombat: ${msg}`);
+	log(msg);
 }
